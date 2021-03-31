04819230395221d7a04fb7b92ae5181ebbbb809a
unknown <phonvanna_author@yahoo.com>
2021-03-31 16:23:26 +0700

    Add signup, login, jwt

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const usersRoute = require('./routes/users');
 const postsRoute = require('./routes/posts');
 
 app.use(bodyParser.json());
 
+app.use("/users", usersRoute);
 app.use("/posts", postsRoute);
-
-
 
 module.exports = app;
